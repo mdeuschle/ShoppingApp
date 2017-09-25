0d2541e3c1872042cc6cb21d08f2ad363de28b1a
Matt Deuschle <mdeuschle@gmail.com>
2017-09-24 22:54:40 -0700

    product VC

---
+++ ShoppingApp/View/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  ShoppingApp
+//
+//  Created by Matt Deuschle on 9/24/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
